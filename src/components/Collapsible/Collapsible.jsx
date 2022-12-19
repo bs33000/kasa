46a0f0a2eb6830f25bfb44daba0b36f6d2ac9ab6
ben <seguret.benoit@gmail.com>
2022-12-19 17:11:10 +0100

    collapsible

---
+++ src/components/Collapsible/Collapsible.jsx
@@ -0,0 +1,41 @@
+import React, {useState, useRef} from "react";
+import Chevron from "../../assets/images/vectorBas.svg";
+
+function Collapsible (props) {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const parentRef = useRef(); //assign value to this DOM element
+
+    return (
+        <div className="collapsible">
+            <button className="toggle" onClick={() => setIsOpen(!isOpen)}>
+                <h2>{props.aboutTitle}</h2>
+                <img
+					className={isOpen ? "chevron rotated" : "chevron"}
+					src={Chevron}
+					alt="chevron"
+				/>
+            </button>
+            <div
+                className="content-parent"
+                ref={parentRef}
+                // conditionnal height: if opened then actual heigh as defined by parent.ref DOM element, 
+                // 0px otherwise - note that in REACt style is an Objet
+                style={
+                    isOpen ? {
+                        height: parentRef.current.scrollHeight + "px"
+                    }
+                    : {
+                        height: "0px"
+                    }
+                }
+            >
+                <div className="content">
+                    <p>{props.aboutText}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Collapsible;
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,7 +1,20 @@
+import AboutBanner from "../../components/AboutBanner/AboutBanner";
+import Collapsible from "../../components/Collapsible/Collapsible";
+import aboutContent from "../../assets/content/aboutContent.json"; // j'ai créé un fichier JSON avec les données des collapses
 import React from "react";
 
 export default function About() {
 	return (
-		<h1>About</h1>
+		<>
+			<AboutBanner />
+			{aboutContent.map((rule, id) => (
+				<Collapsible
+					key={id}
+					aboutTitle={rule.aboutTitle}
+					aboutText={rule.aboutText}
+					aboutStyle="about-style"
+				/>
+			))}
+		</>
 	);
 }
